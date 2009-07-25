0413c13bb057d0ba8278232d6a4a15922b12bbe4
ecavazos <ejcavazos@gmail.com>
2009-07-25 11:01:12 -0700

    added a cleanup method to 'DirSync' class

--- dir_sync.rb
+++ dir_sync.rb
@@ -2,26 +2,47 @@ require 'FileUtils'
 
 class DirSync
   
-  def self.make_destination destination
+  def self.make_dest destination
     FileUtils.mkdir(destination) unless File.directory?(destination)
   end
   
-  def self.sync source, destination
-      changed = false
-      
-      self.make_destination destination
-      
-      Dir.glob(source + '/*').each do |file|
-        tofile = destination + '/' + File.basename(file)
-        
-        if FileUtils.uptodate?(file, tofile)
-          FileUtils.cp_r file, tofile
-          changed = true
-          puts "'#{file}' copied to '#{tofile}'."
-        end
-      end
+  def self.sync source, destination      
+      self.make_dest destination
+      changed = self.copy_new source, destination
+      self.clean_dest source, destination
       
       changed
   end
   
+  def self.copy_new source, destination
+    made_changes = false
+    
+    Dir.foreach(source) { |file|
+      from = File.join(source, file)
+      to = File.join(destination, file)
+      
+      if FileUtils.uptodate?(from, to)
+        FileUtils.cp_r from, to
+        puts "Copied files from #{source} to #{destination}:" unless made_changes
+        made_changes = true
+        puts "  #{file}"
+      end
+    }
+    
+    made_changes
+  end
+  
+  def self.clean_dest source, destination
+    made_changes = false
+    
+    Dir.foreach(destination) { |file| 
+      tmp = File.join(source, file)
+      if !File.exist?(tmp)
+        FileUtils.rm File.join(destination, file)
+        puts "Deleted unmatched file from destination directory:" unless made_changes
+        made_changes = true
+        puts "  #{file}"
+      end
+    }
+  end
 end
